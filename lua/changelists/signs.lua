5bd0119624eeb79ab9b52131086ddebd56647a04
Mohsin <mohsinshk4026@gmail.com>
2024-11-22 15:52:49 +0500

    feat: configurable lists

--- lua/changelists/signs.lua
+++ lua/changelists/signs.lua
@@ -3,22 +3,25 @@
 --- @field name string
 --- @field sign string
 
+local default_list = "changes"
+
 ---@type table<integer, Changelist>
 local lists = {
-	[0] = {
+	[default_list] = {
 		id = 0,
 		name = "Changes",
 		sign = "CL_CHANGES",
+		symbol = "▢",
 	},
 }
 
 local function sign(linenum, cl)
-	print("sign", linenum)
-	cl = cl or lists[0].sign
-	vim.fn.sign_place(linenum, "changelists", cl, "%", { lnum = linenum })
+	cl = lists[cl] or lists[default_list]
+
+	vim.fn.sign_place(linenum, "changelists", cl.sign, "%", { lnum = linenum })
 end
 
-local function setup()
+local function setup(user_lists)
 	local augroup = vim.api.nvim_create_augroup("changelists", { clear = true })
 
 	vim.api.nvim_create_autocmd("ColorScheme", {
@@ -27,13 +30,16 @@ local function setup()
 		command = "highlight String guifg=#FFEB95",
 	})
 
+	lists = vim.tbl_deep_extend("force", lists, user_lists or {})
 	for _, cl in pairs(lists) do
-		vim.fn.sign_define(cl.sign, { text = tostring(cl.id), texthl = cl.name })
-		vim.cmd([[highlight Changes guifg=#C0FFEE]])
+		vim.fn.sign_define(cl.sign, { text = tostring(cl.symbol), texthl = cl.name })
+		vim.cmd([[highlight cl.name guifg=#C0FFEE]])
 	end
 end
 
 return {
 	sign = sign,
 	setup = setup,
+	lists = lists,
+	default = default_list,
 }
